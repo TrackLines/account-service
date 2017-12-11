c8517719196e6d8201d4262684dfd0f8238bdcb3
Max Hooton <max.hooton@auden.co.uk>
2017-12-11 13:30:53 +0000

    pre self-perms

--- api/controllers/accountController.js
+++ api/controllers/accountController.js
@@ -3,53 +3,66 @@
 let mongoose = require('mongoose');
 let Account = mongoose.model('Account');
 
-exports.list_all_accounts = function(req, res) {
-    Account.find({}, function(err, account) {
-        if (err) {
-            res.send(err);
-        }
+let ManagementClient = require('auth0').ManagementClient;
+let auth0 = new ManagementClient({
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    scope: "read:users create:users update:users delete:users"
+});
 
-        res.json(account);
-    });
+let classControl = {
+    list_all_accounts: function(req, res) {
+        auth0.users.getAll(function(err, clients) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(clients);
+        });
+    },
+
+    create_account: function(req, res) {
+        auth0.users.create(req.body, function(err, client) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(client);
+        })
+    },
+
+    get_account: function(req, res) {
+        auth0.users.get(req.params.accountId, function(err, client) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(client);
+        })
+    },
+
+    update_account: function(req, res) {
+        auth0.users.update(req.params.accountId, req.body, function(err, client) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(client);
+        });
+    },
+
+    delete_account: function(req, res) {
+        auth0.users.delete(req.params.accountId, function(err, client) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({
+                message: 'Account Deleted'
+            });
+        });
+    }
 };
 
-exports.create_account = function(req, res) {
-    let new_account = new Account(req.body);
-    new_account.save(function(err, account) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(account);
-    });
-};
-
-exports.get_account = function(req, res) {
-    Account.findById(req.params.accountId, function(err, account) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(account);
-    });
-};
-
-exports.update_account = function(req, res) {
-    Account.findOneAndUpdate({_id: req.params.accountId}, req.body, {new: true}, function(err, account) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(account);
-    });
-};
-
-exports.delete_account = function(req, res) {
-    Account.findOneAndUpdate({_id: req.params.accountId}, {status: 'deleted'}, function(err, account) {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json({message: 'Account Deleted'});
-    });
-};
+module.exports = classControl;
